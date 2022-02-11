380e5c5324f5afa124991b3e4eb32328ae621c98
gfunkmaster <nurkicelmin@hotmail.com>
2022-02-11 15:15:13 +0100

    Iterate throgh, returned new easier name

--- src/context.js
+++ src/context.js
@@ -5,9 +5,48 @@ const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  const [loading, setLoading] = useState(true)
-const [searchTerm, setSearchTerm] = useState('a')
-const [cocktails, setCocktails] = useState([])
+  const [loading, setLoading] = useState(true);
+const [searchTerm, setSearchTerm] = useState('a');
+const [cocktails, setCocktails] = useState([]);
+
+
+// fetch with asyc
+const fetchDrinks = async () => {
+  setLoading(true) // when we want to fetch it's alwas true 
+
+  // try for cocktails
+  try {
+    
+    const resp = await fetch(`${url}${searchTerm}`);
+    const data = await resp.json();
+    console.log(data);
+    const {drinks} = data;
+    if(drinks) {
+      const newCocktails = drinks.map((item) => { //iterate through drinks 
+        const {idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass} = item; //destructing 
+        return {id: idDrink, name : strDrink, image: strDrinkThumb, info: strAlcoholic, glass: strGlass } //renaming when we send
+        setCocktails(newCocktails); // we return the newCocktials
+      })
+
+    } else {
+      setCocktails([]);
+    }
+    setLoading(false); 
+    
+  } catch (error) { 
+    console.log(error);
+    setLoading(false);
+  }
+
+}
+
+//invoking the function 
+useEffect(() => {
+
+  fetchDrinks();
+
+},[searchTerm])
+
   return <AppContext.Provider value={{loading, searchTerm, cocktails, setSearchTerm}}
   >{children}</AppContext.Provider>
 }
